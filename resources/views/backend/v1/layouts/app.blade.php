7a77c572429ec77501803ef282d992aac05db685
Trung Hong Ta <trung.ta@hdwebsoft.co>
2019-07-23 18:48:59 +0700

    .

--- resources/views/backend/v1/layouts/app.blade.php
+++ resources/views/backend/v1/layouts/app.blade.php
@@ -4,7 +4,7 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="csrf-token" content="{{ csrf_token() }}">
-  <title>Đặt Hàng Nhanh Rẻ | Dashboard</title>
+  <title>{{config('shop.name')}} | Dashboard</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->
