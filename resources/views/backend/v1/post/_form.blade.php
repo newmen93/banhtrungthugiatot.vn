ef9e40ed84cbd25c7767c9d8c40f33ed474a8782
Trung Hong Ta <trung.ta@hdwebsoft.co>
2019-07-30 11:33:10 +0700

    .

--- resources/views/backend/v1/post/_form.blade.php
+++ resources/views/backend/v1/post/_form.blade.php
@@ -1,37 +1,22 @@
 <form class="form-horizontal" action="{{route('admin.post.store')}}" method="post" id="post-form">
     <div class="form-group">
-        <label for="name" class="col-sm-2 control-label">Tiêu đề bài viết</label>
+        <label for="title" class="col-sm-2 control-label">Tiêu đề bài viết</label>
         @csrf
         <div class="col-sm-10">
-            <input type="text" class="form-control" name="name" placeholder="Tên danh mục">
+            <input type="text" class="form-control" name="title" placeholder="Tên bài viết">
         </div>
     </div>
     <div class="form-group">
-        <label for="name" class="col-sm-2 control-label">Ảnh thumb</label>
+        <label for="image" class="col-sm-2 control-label">Ảnh thumb</label>
         <div class="col-sm-10">
             <input type="file" class="form-control" name="image">
         </div>
     </div>
     <div class="form-group">
-        <label for="parent" class="col-sm-2 control-label">Nội dung</label>
+        <label for="content" class="col-sm-2 control-label">Nội dung</label>
         <div class="col-sm-10">
-            <textarea name="parent" class="form-control summernote" id="parent">
+            <textarea name="content" class="form-control summernote">
             </textarea>
         </div>
     </div>
-   
-    {{-- @if($mode == "create")
-    <div class="form-group">
-        <div class="col-sm-offset-3 col-sm-9">
-            <button type="submit" class="btn btn-success">Thêm</button>
-        </div>
-    </div>
-    @endif
-    @if($mode == "edit")
-    <div class="form-group">
-        <div class="col-sm-offset-3 col-sm-9">
-            <button type="submit" class="btn btn-success">Lưu</button>
-        </div>
-    </div>
-    @endif --}}
 </form>
